9be6da3cf7d7971393c8f3c9a8d7a960b6c15eb2
YuvamKumarSWE <yuvamk.swe@gmail.com>
2025-05-27 23:13:33 -0400

    Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login(){
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error , setError] = useState("");
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setError("");
+        //validaton for password
+
+        try{
+            // backend login
+            navigate("/dashboard")
+        } catch(error){
+            setError(error);
+            console.log(error);
+        }
+    }
+
     return(
         <>
             <form onSubmit={()=>{console.log("Hi")}}>
                 
-                <input type="email" placeholder="Email" value={email}  />
+                <input type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} className="input-box" />
 
-                <input type="password" name="" id="" />
+                <input type="password" name="" id="" hidden onChange={(e)=>setPassword(e.target.value)} className="input-box"/>
             </form>
         </>
     );
